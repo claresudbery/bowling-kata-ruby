563e11ca06370d1b6d47083794c89c40837c07a1
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-11 17:29:51 +0100

    refactor

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -2,14 +2,11 @@ require_relative '../src/bowling'
 
 RSpec.describe Bowling do
 
-    it 'creates a Bowling class' do
-        bowling = Bowling.new
-        expect(bowling).to be_kind_of(Bowling)
-    end
-
     it 'returns zero score when all rolls are misses' do
         bowling = Bowling.new
-        expect(bowling.score("-- -- -- -- -- -- -- -- -- --")).to eq(0)
+        zero_scoring_rolls = "-- -- -- -- -- -- -- -- -- --"
+        
+        expect(bowling.score(zero_scoring_rolls)).to eq(0)
     end
     
 end
