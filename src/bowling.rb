185db731a1683535b020de88b7b238a7e3f5ca34
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-11 17:37:56 +0100

    Now calculating actual scores

--- src/bowling.rb
+++ src/bowling.rb
@@ -1,7 +1,15 @@
 class Bowling
 
     def score(rolls)
-        0
+        frames = rolls.split(" ")
+        score = 0
+
+        frames.each do |item|
+            item_score = item[0].to_i + item[1].to_i
+            score = score + item_score
+        end
+        
+        score
     end
-    
+
 end
