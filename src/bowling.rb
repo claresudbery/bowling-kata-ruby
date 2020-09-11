6606cf1f7ea7f3b77ed4de34f56e4f8641ae9ce7
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-11 19:14:13 +0100

    refactor

--- src/bowling.rb
+++ src/bowling.rb
@@ -5,12 +5,12 @@ class Bowling
     def score(rolls)
         frames = rolls.split(" ")
         score = 0
-        item_score = 0
+        frame_score = 0
 
         frames.each do |frame|
-            item_score = spare_score(item_score, frame)
-            item_score += add_frame_pins(frame)
-            score = score + item_score
+            frame_score = spare_score(frame_score, frame)
+            frame_score += add_frame_pins(frame)
+            score = score + frame_score
         end
         
         score
@@ -18,8 +18,14 @@ class Bowling
 
     private
 
-    def spare_score(previous_frame_score, this_frame)        
-        spare_score = (previous_frame_score == ALL_PINS) ? add_frame_pins(this_frame) : 0
+    def spare_score(previous_frame_score, this_frame)    
+        spare_score = 0    
+
+        if previous_frame_score == ALL_PINS 
+            spare_score = add_frame_pins(this_frame)
+        end
+
+        spare_score
     end
 
     def add_frame_pins(frame)
