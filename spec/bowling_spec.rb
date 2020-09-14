d0c873832483877707ddb8871b986d3d026e22a6
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-14 20:24:32 +0100

    Remove test cases for misunderstood scoring rules, add failing test for correct spare scoring rule.

--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -24,42 +24,11 @@ RSpec.describe Bowling do
     end
 
     expected_scores_with_spares_no_strikes = {
-        "55 44 44 44 44 44 44 44 44 44" => 10 + 8 + (9 * 8),
-        "55 55 44 44 44 44 44 44 44 44" => (10 * 2) + (10 + 8) + (8 * 8),
-        "44 44 55 55 44 44 44 44 44 44" => (10 * 2) + (10 + 8) + (8 * 8),
-        "44 44 46 91 44 44 44 44 44 44" => (10 * 2) + (10 + 8) + (8 * 8),
-        "42 62 37 14 55 91 33 28 11 11" => (5 + 10 + 6 + 2) + (6 + 8 + 10 + 5 + 10 + 10 + 6 + 10 + 2 + 2),
-        "4- -2 37 14 55 91 33 28 -- 11" => (5 + 10 + 6 + 0) + (4 + 2 + 10 + 5 + 10 + 10 + 6 + 10 + 0 + 2)
+        "55 44 44 44 44 44 44 44 44 44" => 10 + 4 + (9 * 8)
     }
 
     expected_scores_with_spares_no_strikes.each do |rolls, score|
         it "adds ten to the score, plus pins from the next frame, when all pins felled in one frame: '#{rolls}'" do
-            bowling = Bowling.new            
-            expect(bowling.score(rolls)).to eq(score)
-        end
-    end
-
-    expected_scores_with_strikes_no_spares = {
-        "X 44 44 44 44 44 44 44 44 44" => 10 + (2*8) + (9*8),
-        "X X 44 44 44 44 44 44 44 44" => (10*2) + ((10+8) + (8+8)) + (8*8),
-        "44 44 X X 44 44 44 44 44 44" => (10*2) + ((10+8) + (8+8)) + (8*8),
-        "42 62 X 14 X X 33 X 11 11" => ((5+10) + (10+6) + (6+10) + (2+2)) + (6+8+10+5+10+10+6+10+2+2),
-        "4- -2 X 14 X X 33 X -- 11" => ((5+10) + (10+6) + (6+10) + (0+2)) + (4+2+10+5+10+10+6+10+0+2)
-    }
-
-    expected_scores_with_strikes_no_spares.each do |rolls, score|
-        it "adds ten to the score, plus pins from the next two frames, when there is a strike: '#{rolls}'" do
-            bowling = Bowling.new            
-            expect(bowling.score(rolls)).to eq(score)
-        end
-    end
-
-    expected_scores_with_strikes_and_spares = {
-        "X 44 55 44 X 44 55 44 X 44" => (10*5) + ((8+10)+(8+10)+8) + (2*8) + (5*8)
-    }
-
-    expected_scores_with_strikes_and_spares.each do |rolls, score|
-        it "adds ten to the score, plus pins from the next two frames, when there is a strike: '#{rolls}'" do
             bowling = Bowling.new            
             expect(bowling.score(rolls)).to eq(score)
         end
